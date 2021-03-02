05c7e308d5ab301b8803ee4920414885ff2e531a
Vieriu Alexandru <vieriu.alexandru.25@gmail.com>
2021-03-02 15:45:59 +0200

    84. Creating Data

---
+++ BlazorClient/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient 
+            { 
+                BaseAddress = new Uri("https://localhost:44345/") 
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ BlazorServer/Models/OrderUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServer.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "The name need to be max 20 char and min 3 characters", MinimumLength = 3)]
+        [DisplayName("Name for the Order")]
+        public string OrderName { get; set; }
+    }
+}
